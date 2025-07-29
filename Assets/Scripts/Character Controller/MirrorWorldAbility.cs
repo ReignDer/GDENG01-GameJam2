70dacd957cc33e61dfff5b3aeaf1ac1ae8a08471
Mathieu Marc Pobre <mathieu_marc_pobre@dlsu.edu.ph>
2025-07-29 22:43:14 +0800

    Skybox tint color change when worlds switch

--- Assets/Scripts/Character Controller/MirrorWorldAbility.cs
+++ Assets/Scripts/Character Controller/MirrorWorldAbility.cs
@@ -2,36 +2,59 @@ using UnityEngine;
 
 public class MirrorWorldAbility : MonoBehaviour
 {
+    private static readonly int TINT_PROPERTY = Shader.PropertyToID("_Tint");
+    private static readonly int SKYTINT_PROPERTY = Shader.PropertyToID("_SkyTint");
     [SerializeField] private InputReader inputReader;
 
     [SerializeField] private GameObject NormalWorld;
     [SerializeField] private GameObject MirrorWorld;
+    [SerializeField] private Light NormalLight;
+    [SerializeField] private Light MirrorLight;
+
+    private Color NormalSkyColor;
+    private Color MirrorSkyColor;
     
     bool isWorldSwitched = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        inputReader.Mirror += SwitchWorld; 
-            
+        inputReader.Mirror += SwitchWorld;
+        
+        if(RenderSettings.skybox.HasProperty(TINT_PROPERTY))
+            NormalSkyColor = RenderSettings.skybox.GetColor(TINT_PROPERTY);
+        else if(RenderSettings.skybox.HasProperty(SKYTINT_PROPERTY))
+            NormalSkyColor = RenderSettings.skybox.GetColor(SKYTINT_PROPERTY);
+        
+        MirrorSkyColor = new Color(188f/255f, 84f/255f, 84f/255f, 1f);
     }
 
     private void SwitchWorld(bool isKeyPressed)
     {
-        if (isKeyPressed)
+        if (!isKeyPressed) return;
+        if (!isWorldSwitched)
         {
-            if (!isWorldSwitched)
-            {
-                MirrorWorld.SetActive(true);
-                NormalWorld.SetActive(false);
-                isWorldSwitched = true;
-            }
-            else
-            {
-                NormalWorld.SetActive(true);
-                MirrorWorld.SetActive(false);
-                isWorldSwitched = false;
-            }
+            MirrorWorld.SetActive(true);
+            NormalWorld.SetActive(false);
+            RenderSettings.sun = MirrorLight;
+            RenderSwitchedWorld(MirrorSkyColor);
+            isWorldSwitched = true;
         }
+        else
+        {
+            NormalWorld.SetActive(true);
+            MirrorWorld.SetActive(false);
+            RenderSettings.sun = NormalLight;
+            RenderSwitchedWorld(NormalSkyColor);
+            isWorldSwitched = false;
+        }
+    }
+
+    void RenderSwitchedWorld(Color tintColor)
+    {
+        if (RenderSettings.skybox.HasProperty(TINT_PROPERTY))
+            RenderSettings.skybox.SetColor(TINT_PROPERTY, tintColor);
+        else if (RenderSettings.skybox.HasProperty(SKYTINT_PROPERTY))
+            RenderSettings.skybox.SetColor(SKYTINT_PROPERTY, tintColor);
     }
 
 }
